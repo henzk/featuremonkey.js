00c68466bf477bb6d351ff2c6ce71a526c47e288
Hendrik Speidel <speidel@schnapptack.de>
2012-10-19 18:12:06 +0200

    -some more tests -removed crap

--- test_featuremonkey.js
+++ test_featuremonkey.js
@@ -126,104 +126,35 @@ $(document).ready(function(){
         equal(base.func_i(), 42, 'func_i returns 42 after compose')
     })
 
+    test("refine function and use original", function() {
+        var base = get_base_feature()
+
+        var fst = {
+            refine_func_i: function(original) {
+                return function() {return original() + 42}
+            }
+        }
+
+        equal(base.func_i(), 1, 'func_i returns 1 before compose')
+        featuremonkey.compose(fst, base)
+        equal(base.func_i(), 43, 'func_i returns 43 after compose')
+    })
+
+    test("refine non-existing function", function() {
+        var base = get_base_feature()
+
+        var fst = {
+            refine_func_xiii: function(original) {
+                return function() {return original() + 42}
+            }
+        }
+
+        throws(
+            function() {
+                featuremonkey.compose(fst, base)
+            },
+            'refined non-existing structure(and got exception)'
+        )
+    })
+
 })
-
-
-
-/*
-    test("tags", function() {
-        equal(curdsoap.clean('<strong>hello</strong>'),
-            '<strong>hello</strong>',
-            'allowed tag containing text'
-        )
-        equal(curdsoap.clean('<muha>hallo</muha>'),
-            'hallo ',
-            'disallowed tag containing text'
-        )
-        equal(curdsoap.clean('<strong><u>hello</u>muha</strong>'),
-            '<strong><u>hello</u>muha</strong>',
-            'multiple allowed tags containing text'
-        )
-        equal(curdsoap.clean('<strong2><u2>hello</u2>muha</strong2>'),
-            'hello muha ',
-            'multiple disallowed tags containing text'
-        )
-        equal(curdsoap.clean('<strong><u2>hello</u2>muha</strong>test'),
-            '<strong>hello muha</strong>test',
-            'multiple disallowed tags containing text'
-        )
-    })
-
-    test("attributes", function() {
-        equal(curdsoap.clean('<spam foo="bar">hello</spam>'),
-            'hello ',
-            'disallowed attribute in disallowed tag without allowed attrs'
-        )
-        equal(curdsoap.clean('<strong foo="bar">hello</strong>'),
-            '<strong>hello</strong>',
-            'disallowed attribute in allowed tag without allowed attrs'
-        )
-        equal(curdsoap.clean('<a foo="bar">hello</a>'),
-            '<a>hello</a>',
-            'disallowed attribute in allowed tag with allowed attrs'
-        )
-        equal(curdsoap.clean('<a href="bar">hello</a>'),
-            '<a href="bar">hello</a>',
-            'allowed attribute in allowed tag'
-        )
-        equal(curdsoap.clean('<a href="bar" title="foo">hello</a>'),
-            '<a href="bar" title="foo">hello</a>',
-            'two allowed attributes in allowed tag'
-        )
-        equal(curdsoap.clean('<a href="bar" title="foo" alt="eggs">hello</a>'),
-            '<a href="bar" alt="eggs" title="foo">hello</a>',
-            'three allowed attributes in allowed tag'
-        )
-    })
-
-    test("ignored nodes", function() {
-        equal(curdsoap.clean('<style>style</style>'),
-            '',
-            'style node'
-        )
-    })
-
-    function xss() {
-        console.log('XSS! PANIC!!!')
-        fail('XSS!! PANIC!!!!')
-    }
-
-    test("XSS", function() {
-        equal(curdsoap.clean('<script>xss()</'+'script>'),
-            'xss()',
-            'script node'
-        )
-    })
-    */
-
-/*
-var a = {}
-var b = {
-    introduce_b: 123,
-    introduce_f: function(self) {
-        console.log('FFFF', self)
-        return function() {
-            console.log('GGGGG', self)
-        }
-    }
-}
-var c = {
-    refine_b: function(original) {
-        return original + 123
-    },
-    refine_f: function(original, self) {
-        return function() {
-            console.log('yeeeha')
-            console.log(self)
-            original()
-        }
-    }
-}
-
-featuremonkey.compose(c,b,a)
-console.log(a)*/
