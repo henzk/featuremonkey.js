56f7c97b3e716fde7456ae8e484dabd20b02cdb5
Hendrik Speidel <speidel@schnapptack.de>
2012-10-26 18:55:20 +0200

    added jsdoc parseable docstrings

--- featuremonkey.js
+++ featuremonkey.js
@@ -1,35 +1,63 @@
 /**
+ *  @fileOverview
  *  featuremonkey.js - FOP for JavaScript
  *  https://github.com/henzk/featuremonkey.js
- *
- *  (c) Hendrik Speidel - http://schnapptack.de/
  *  License: MIT
+ *  @author Hendrik Speidel - http://schnapptack.de/
+ *
+ *  @description
+ *  <tt>featuremonkey.js</tt> is a tool to build feature 
+ *  oriented javascript application product lines. It is a port of
+ *  <a href="http://github.com/henzk/featuremonkey">featuremonkey</a>
+ *  from python to JavaScript.
+ *  <br>
+ *  The project can be found online at 
+ *  <a href="http://github.com/henzk/featuremonkey.js">
+ *  http://github.com/henzk/featuremonkey.js</a>.
+ **/
+
+/**
+ *  Most times, it should be sufficient to use only 
+ *  <tt>featuremonkey.compose</tt> to superimpose FSTs and
+ *  an implementation.
+ *  <br>
+ *  All functions prefixed with <tt>_</tt> are for advanced usage only.
+ *  @see featuremonkey#compose
+ *  @namespace
  **/
 var featuremonkey = {}
 
 /**
- *  thrown when things go south during composition
- *  seems best to use simple strings here - there seems to be no
- *  way to inspect thrown objects in webkit inspector...
+ *  @description
+ *  thrown when things go south during composition.
+ *  Unfortunately, there seems to be no way to
+ *  inspect thrown objects in webkit inspector.
+ *  Therefore, this is currently returns a string.
+ *  
+ *  @param {string} msg  the error message
+ *  
+ *  @returns {string} composition error message
  **/
 featuremonkey.CompositionError = function(msg) {
     return 'featuremonkey.CompositionError: ' + msg
 }
 
 /**
- *  refine a property
- *
- *  current_impl - the object containing the property
- *  attribute_name - the name of the property to refine
- *  refinement - refinement to apply to the property
+ *  @public
+ *  @description
+ *  refine a property.
  *
  *  If refinement is not a function, its value is used as 
  *  the refined implementation of the property.
- *  If refinement is a function `func`, `func(original, self)` is called 
+ *  If refinement is a function <tt>func</tt>, <tt>func(original, self)</tt> is called 
  *  to obtain the refined implementation of the property.
- *  `original` is the original implementation of the property;
- *  `self` is a reference to the object containing the property
+ *  <tt>original</tt> is the original implementation of the property;
+ *  <tt>self</tt> is a reference to the object containing the property
  *  and may be useful when refining methods.
+ *
+ *  @param {object} current_impl  the object containing the property
+ *  @param {string} attribute_name  the name of the property to refine
+ *  @param {object} refinement  refinement to apply to the property
  **/
 featuremonkey._refine = function(current_impl, attribute_name, refinement) {
     if (typeof refinement == 'function') {
@@ -40,19 +68,21 @@ featuremonkey._refine = function(current_impl, attribute_name, refinement) {
 }
 
 /**
- * introduce a property
- *
- *  current_impl - the object containing the property
- *  attribute_name - the name of the property to refine
- *  introduction - description of the property to introduce
+ *  @public
+ *  @description
+ *  introduce a property.
  *
  *  Introductions are specified analogous to refinements.
  *  If introduction is not a function, its value is used as 
  *  value of the new property.
- *  If introduction is a function `func`, `func(self)` is called 
+ *  If introduction is a function <tt>func</tt>, <tt>func(self)</tt> is called 
  *  to obtain the new value of the property.
- *  `self` is a reference to the object containing the property
+ *  <tt>self</tt> is a reference to the object containing the property
  *  and may be useful when introducing methods.
+ *
+ *  @param {object} current_impl the object containing the property
+ *  @param {string} attribute_name the name of the property to refine
+ *  @param {object} introduction description of the property to introduce
  **/
 featuremonkey._introduce = function(current_impl, attribute_name, introduction) {
     if (typeof introduction == 'function') {
@@ -63,12 +93,14 @@ featuremonkey._introduce = function(current_impl, attribute_name, introduction) 
 }
 
 /**
+ *  @public
+ *  @description
  *  refine a factory function - the resulting factory will 
- *  apply the given fst to all produced objects
+ *  apply the given fst to all produced objects.
  *
- *  current_impl - the object that contains the factory function
- *  attribute_name - the name of the property of the factory function in current_impl
- *  element_fst - the fst that needs to be applied to objects produced by the factory
+ *  @param {object} current_impl  the object that contains the factory function
+ *  @param {string} attribute_name  the name of the property of the factory function in current_impl
+ *  @param {object} element_fst  the fst that needs to be applied to objects produced by the factory
  **/
 featuremonkey._factory_refine = function(current_impl, attribute_name, element_fst) {
     var original = current_impl[attribute_name]
@@ -82,9 +114,14 @@ featuremonkey._factory_refine = function(current_impl, attribute_name, element_f
 }
 
 /**
- * compose a fst and an implementation
+ *  @public
+ *  @description
+ *  compose a fst and an implementation.
  *
- * superimposable_fst is placed on current_impl
+ *  This places <tt>superimposable_fst</tt> on top of <tt>current_impl</tt>.
+ *  
+ *  @param {object} superimposable_fst  fst that is placed on an implementation
+ *  @param {object} current_impl  implementation that the fst is placed on
  **/
 featuremonkey._compose = function(superimposable_fst, current_impl) {
 
@@ -123,12 +160,15 @@ featuremonkey._compose = function(superimposable_fst, current_impl) {
 }
 
 /**
- * compose multiple fsts and an implementation
+ *  @description
+ *  compose multiple fsts and an implementation.
  *
- * the implementation must be the last parameter preceded by one or multiple fsts
- * fsts are composed from right to left - the leftmost fst is composed last!
+ *  @param {...} varargs  the implementation must be the last parameter preceded by one or multiple fsts
+ *  fsts are composed from right to left - the leftmost fst is composed last!
+ *  
+ *  @throws {string} CompositionError
  **/
-featuremonkey.compose = function() {
+featuremonkey.compose = function(varargs) {
 
     var args = Array.prototype.slice.call(arguments)
     var base_impl = args.pop()
